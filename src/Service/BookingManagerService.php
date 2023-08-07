a1702575319dc1114a807893a8a6d5f17f74365d
kouwa stephane <kksasteph888@gmail.com>
2023-08-07 19:09:17 +0200

    Maj

--- src/Service/BookingManagerService.php
+++ src/Service/BookingManagerService.php
@@ -6,32 +6,29 @@ use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\booking_system\Entity\BookingReservation;
-use Entity;
-use Google\Service\CloudComposer\ScheduledSnapshotsConfig;
-use Google\Service\Kgsearch\Resource\Entities;
 
 /**
  * Manage the booking system
  */
-class BookingManagerService extends ControllerBase
-{
-
+class BookingManagerService extends ControllerBase {
   protected $currentUser;
-  protected $em;
+  /**
+   *
+   * @var EntityTypeManager
+   */
+  protected $entityTypeManager;
 
-  public function __construct(AccountInterface $currentUser, EntityTypeManager $em)
-  {
+  public function __construct(AccountInterface $currentUser, EntityTypeManager $entityTypeManager) {
     $this->currentUser = $currentUser;
-    $this->em = $em;
+    $this->entityTypeManager = $entityTypeManager;
   }
 
   /**
-   * 
+   *
    * {@inheritdoc}
-   * 
+   *
    */
-  public function generateDates()
-  {
+  public function generateDates() {
     $data = [];
     $config = $this->config('booking_system.settings')->getRawData();
     $data["globalDiscount"] = $config["reduction"];
@@ -44,36 +41,48 @@ class BookingManagerService extends ControllerBase
      *
      * @var \Drupal\booking_system\Entity\BookingDateEntity $entity
      */
-    $disabledDays = $this->em->getStorage('booking_system_date')->loadMultiple(); //getQuery permet de construire une requête
-    /*$disabledDates = $this->getDisabledDates($disabledDays);
-    $data['disabledDates'] = $disabledDates;*/
-    //dump($disabledDays);
+    $disabledDays = $this->entityTypeManager->getStorage('booking_system_date')->loadMultiple(); // getQuery
+                                                                                                 // permet
+                                                                                                 // de
+                                                                                                 // construire
+                                                                                                 // une
+                                                                                                 // requête
+    /*
+     * $disabledDates = $this->getDisabledDates($disabledDays);
+     * $data['disabledDates'] = $disabledDates;
+     */
+    // dump($disabledDays);
     return $data;
   }
 
   /**
-   * 
+   *
    * {@inheritdoc}
-   * 
+   *
    */
-  public function generateDisabledDates(){
-    $data=[];
-    $entities = $this->em->getStorage('booking_system_date')->loadMultiple(); //getQuery permet de construire une requête
+  public function generateDisabledDates() {
+    $data = [];
+    $entities = $this->entityTypeManager->getStorage('booking_system_date')->loadMultiple(); // getQuery
+                                                                                             // permet
+                                                                                             // de
+                                                                                             // construire
+                                                                                             // une
+                                                                                             // requête
     $today = strtotime('today');
     /**
      *
      * @var \Drupal\booking_system\Entity\BookingSystemDate $entity
      */
-    foreach($entities as $entity){
+    foreach ($entities as $entity) {
       $date_status = $entity->get("status")->getValue()[0]['value'];
-      $date_value  = strtotime($entity->get("start_date")->getValue()[0]['value']);
-      if(!$date_status && $date_value >= $today){
-        $data[] =  $date_value;
+      $date_value = strtotime($entity->get("start_date")->getValue()[0]['value']);
+      if (!$date_status && $date_value >= $today) {
+        $data[] = $date_value;
       }
     }
-    //check if today has a valid schedule
-    if(!$this->hasValidSchedule($today)){
-      if(!in_array($today, $data) ){
+    // check if today has a valid schedule
+    if (!$this->hasValidSchedule($today)) {
+      if (!in_array($today, $data)) {
         $data[] = $today;
       }
     }
@@ -81,12 +90,11 @@ class BookingManagerService extends ControllerBase
   }
 
   /**
-   * 
+   *
    * {@inheritdoc}
-   * 
+   *
    */
-  public function generateSchdules($day)
-  {
+  public function generateSchdules($day) {
     $data = [];
 
     $dayNames = [
@@ -96,13 +104,13 @@ class BookingManagerService extends ControllerBase
       'Wednesday' => 3,
       'Thursday' => 4,
       'Friday' => 5,
-      'Saturday' => 6,
+      'Saturday' => 6
     ];
 
     // retrive the global config alues
     $config = $this->config('booking_system.settings')->getRawData();
 
-    //very if the date is valid
+    // very if the date is valid
     $today = strtotime("today");
     if ($day < $today) {
       $data["error"] = "The day sent must be posterior to the current day";
@@ -115,10 +123,9 @@ class BookingManagerService extends ControllerBase
       $data["error"] = "The day sent must be in a interval time of $numberOfDays from the current date";
       return $data;
     }
-    //check if the date is modified specificaly
+    // check if the date is modified specificaly
 
-
-    //else get the default configuration and render it
+    // else get the default configuration and render it
     $dayOfTheWeek = date('l', $day);
     $isValidated = $config["jours"][$dayNames[$dayOfTheWeek]]["status"];
     if ($isValidated != 1) {
@@ -135,47 +142,47 @@ class BookingManagerService extends ControllerBase
       $dayPeriod['discount'] = -$period['reduction'];
       $temp = $this->getPeriodes($period["h_d__m_d"], $period["h_f__m_f"], (int) $period["intervalle"], $day, $period["decallage"]);
 
-      foreach($temp as $hour){
+      foreach ($temp as $hour) {
         $time_hour = strtotime($hour) - strtotime("today");
-        $state = $this->periodIsValid($day, $time_hour, $time_hour + $period['intervalle']*60, $period['decallage'], $period['intervalle']);
-        $state = $this->getSeats($day, $hour)['number'] >= 1 && $state==true? true: false;
-        $dayPeriod["times"][]= [
+        $state = $this->periodIsValid($day, $time_hour, $time_hour + $period['intervalle'] * 60, $period['decallage'], $period['intervalle']);
+        $state = $this->getSeats($day, $hour)['number'] >= 1 && $state == true ? true : false;
+        $dayPeriod["times"][] = [
           'hour' => $hour,
-          'status' => $state,
-        ]; 
+          'status' => $state
+        ];
       }
 
-      $periods[] = $dayPeriod;  
+      $periods[] = $dayPeriod;
     }
     // $datas = [];
     // //range of hours to disabled
-    // $entities = $this->em->getStorage('booking_system_schedule')->loadMultiple();
+    // $entities =
+    // $this->entityTypeManager->getStorage('booking_system_schedule')->loadMultiple();
     // /**
-    //  *
-    //  * @var \Drupal\booking_system\Entity\BookingSystemSchedule $entity
-    //  */    
+    // *
+    // * @var \Drupal\booking_system\Entity\BookingSystemSchedule $entity
+    // */
     // foreach($entities as $entity){
-    //   $schedules = $entity->get("schedule")->getValue(); 
-    //   /**
-    //   *
-    //   * @var \Drupal\booking_system\Plugin\Field\FieldType\ScheduleItem $schedule
-    //   */
-    //   foreach($schedules as $schedule){      
-    //     $datas[] = $schedule;
-    //   }
+    // $schedules = $entity->get("schedule")->getValue();
+    // /**
+    // *
+    // * @var \Drupal\booking_system\Plugin\Field\FieldType\ScheduleItem
+    // $schedule
+    // */
+    // foreach($schedules as $schedule){
+    // $datas[] = $schedule;
+    // }
     // }
 
     return $periods;
   }
 
   /**
-   * 
-   * {@inheritdoc}
-   * get the disabled days
-   * 
+   *
+   * {@inheritdoc} get the disabled days
+   *
    */
-  public function getDisabledDaysOfTheWeek(array $days)
-  {
+  public function getDisabledDaysOfTheWeek(array $days) {
     $disabledDays = [];
     foreach ($days as $k => $day) {
       if ($day["status"] == 0) {
@@ -183,16 +190,15 @@ class BookingManagerService extends ControllerBase
       }
     }
     $disabledDays = array_values($disabledDays);
-    //dump($disabledDays);
+    // dump($disabledDays);
     return $disabledDays;
   }
 
   /**
-   * 
+   *
    * {@inheritdoc}
-   * 
+   *
    */
-
   public function getDisabledDates(array $dates) {
     $disabledDates = [];
     foreach ($dates as $d => $date) {
@@ -206,12 +212,10 @@ class BookingManagerService extends ControllerBase
   }
 
   /**
-   * 
-   * {@inheritdoc}
-   * get the periods
+   *
+   * {@inheritdoc} get the periods
    */
-  public function getPeriodes($start, $end, $gap, $day, $intervalle)
-  {
+  public function getPeriodes($start, $end, $gap, $day, $intervalle) {
     $intervalle = (int) $intervalle;
 
     $times = [];
@@ -232,58 +236,57 @@ class BookingManagerService extends ControllerBase
 
     return $times;
   }
-  /**
-   * 
-   * {@inheritdoc}
-   * check the validity of a period
-   */
-  public function periodIsValid($day, $start_hour, $end_hour, $gap = 15, $intervalle=15){
-    $isValid = true;
-    $current_schedule =[
-      'start' => $day + $start_hour,
-      'end'   =>$day + $end_hour
-    ];
-    //get the current time + 6hours (to match timezone)    
-    $currentTime = strtotime("now") ;
 
-    //checking if the hour isn't passed yet
-    if($current_schedule['start'] <= ($currentTime +$gap*60)){
+  /**
+   *
+   * {@inheritdoc} check the validity of a period
+   */
+  public function periodIsValid($day, $start_hour, $end_hour, $gap = 15, $intervalle = 15) {
+    $isValid = true;
+    $current_schedule = [
+      'start' => $day + $start_hour,
+      'end' => $day + $end_hour
+    ];
+    // get the current time + 6hours (to match timezone)
+    $currentTime = strtotime("now");
+
+    // checking if the hour isn't passed yet
+    if ($current_schedule['start'] <= ($currentTime + $gap * 60)) {
       return false;
     }
 
-    //range of hours to disabled
-    $entities = $this->em->getStorage('booking_system_schedule')->loadMultiple();
+    // range of hours to disabled
+    $entities = $this->entityTypeManager->getStorage('booking_system_schedule')->loadMultiple();
     /**
      *
      * @var \Drupal\booking_system\Entity\BookingSystemSchedule $entity
-     */    
-    foreach($entities as $entity){
-      $schedules = $entity->get("schedule")->getValue(); 
+     */
+    foreach ($entities as $entity) {
+      $schedules = $entity->get("schedule")->getValue();
       /**
-      *
-      * @var \Drupal\booking_system\Plugin\Field\FieldType\ScheduleItem $schedule
-      */
-      foreach($schedules as $schedule){
-        $start  = $schedule['start_date'] + strtotime($schedule['start_hour']) - strtotime('today');
-        $end    = $schedule['end_date'] + strtotime($schedule['end_hour']) - strtotime('today');
-        //check if the schedule is in the range of de disabled range of hours
-        if($current_schedule['start'] >= $start && $current_schedule['start'] <$end ||
-           $current_schedule['end'] >= $start && $current_schedule['end'] <= $end){
+       *
+       * @var \Drupal\booking_system\Plugin\Field\FieldType\ScheduleItem $schedule
+       */
+      foreach ($schedules as $schedule) {
+        $start = $schedule['start_date'] + strtotime($schedule['start_hour']) - strtotime('today');
+        $end = $schedule['end_date'] + strtotime($schedule['end_hour']) - strtotime('today');
+        // check if the schedule is in the range of de disabled range of hours
+        if ($current_schedule['start'] >= $start && $current_schedule['start'] < $end || $current_schedule['end'] >= $start && $current_schedule['end'] <= $end) {
           $isValid = false;
-          break;   
+          break;
         }
       }
-      if(!$isValid)
+      if (!$isValid)
         break;
-    }    
+    }
     return $isValid;
   }
 
   /**
+   *
    * {@inheritdoc}
    * @param array $reservation
-   * @throws \Exception
-   * set the reservations
+   * @throws \Exception set the reservations
    */
   public function setRerservations(array $reservation) {
     $user_id = $this->currentUser->id();
@@ -295,91 +298,89 @@ class BookingManagerService extends ControllerBase
         'time_of_reservation' => $reservation['time_of_reservation'],
         'periode_name' => $reservation['periode_name'],
         'reservation_date' => $reservation['reservation_date'],
-        'reservation_reduction' => $reservation['reservation_reduction'],
+        'reservation_reduction' => $reservation['reservation_reduction']
       ]);
       return $reservation->save();
     }
     throw new \Exception("You must be logged in to be able to");
   }
-  
+
   /**
+   *
    * {@inheritdoc}
    * @param array $reservation
-   * @throws \Exception
-   * set the reservations
+   * @throws \Exception set the reservations
    */
-  
-  public function reservationIsValid(array $reservation){
+  public function reservationIsValid(array $reservation) {
     $schedules = $this->generateSchdules($reservation['reservation_date']);
     $text_to_throw = 'Invalids datas';
-    //how we expect to find our schedule in the array. 
+    // how we expect to find our schedule in the array.
     $expected_hour = [
       'hour' => $reservation['time_of_reservation'],
       'status' => true
     ];
-    //will get True if the hour exist
+    // will get True if the hour exist
     $hour_exist = false;
-    
-    //will get 'true' if the period_name received is valid
+
+    // will get 'true' if the period_name received is valid
     $period_exist = false;
 
     $period_name = $reservation['periode_name'];
-    
-    //start verifications
-    if('integer' != gettype($reservation['reservation_reduction'])){
-      throw new \Exception($text_to_throw.". Discout value not valid");
+
+    // start verifications
+    if ('integer' != gettype($reservation['reservation_reduction'])) {
+      throw new \Exception($text_to_throw . ". Discout value not valid");
     }
-    
-    if('integer' != gettype($reservation['number_of_places'])){
-      throw new \Exception($text_to_throw.". Seats number not valid");
+
+    if ('integer' != gettype($reservation['number_of_places'])) {
+      throw new \Exception($text_to_throw . ". Seats number not valid");
     }
     $valid_dates = $this->generateDisabledDates();
-    if(in_array($reservation["reservation_date"], $valid_dates)){
-      throw new \Exception($text_to_throw.". Date not valid");
+    if (in_array($reservation["reservation_date"], $valid_dates)) {
+      throw new \Exception($text_to_throw . ". Date not valid");
     }
-    foreach($schedules as $period){ 
-      if($period["name"] == $period_name){
+    foreach ($schedules as $period) {
+      if ($period["name"] == $period_name) {
         $period_exist = true;
         break;
       }
-
     }
 
-    if(!$period_exist){
-      throw new \Exception($text_to_throw." Period name not valid");
+    if (!$period_exist) {
+      throw new \Exception($text_to_throw . " Period name not valid");
     }
 
-    foreach($schedules as $period){
-      if(in_array($expected_hour, $period['times'])){
+    foreach ($schedules as $period) {
+      if (in_array($expected_hour, $period['times'])) {
         $hour_exist = true;
         break;
       }
     }
 
-    if(!$hour_exist){
-      throw new \Exception($text_to_throw.". Given hour not valid");
+    if (!$hour_exist) {
+      throw new \Exception($text_to_throw . ". Given hour not valid");
     }
-    if($this->getSeats($reservation['reservation_date'], $reservation['time_of_reservation'])['number'] < $reservation['number_of_places']){
-      throw new \Exception($text_to_throw.". (number of seat) Given number of seats > number of seats available");
+    if ($this->getSeats($reservation['reservation_date'], $reservation['time_of_reservation'])['number'] < $reservation['number_of_places']) {
+      throw new \Exception($text_to_throw . ". (number of seat) Given number of seats > number of seats available");
     }
     return true;
   }
 
-   /**
-   * {@inheritdoc}
-   * check if the date contain at least one valid schedule
+  /**
+   *
+   * {@inheritdoc} check if the date contain at least one valid schedule
    */
-  private function hasValidSchedule($day){
+  private function hasValidSchedule($day) {
     $schedules = $this->generateSchdules($day);
     $status = false;
     foreach ($schedules as $periods) {
       foreach ($periods['times'] as $schedule) {
-        if($schedule['status']){
+        if ($schedule['status']) {
           $status = true;
           break;
         }
       }
-      if($status){
+      if ($status) {
         break;
       }
     }
@@ -387,31 +388,30 @@ class BookingManagerService extends ControllerBase
   }
 
   /**
-   * {@inheritdoc}
-   * get how many seats left to be reserved in an hour of a day
+   *
+   * {@inheritdoc} get how many seats left to be reserved in an hour of a day
    */
-  function getSeats($day, $hour){
+  function getSeats($day, $hour) {
     $config = $this->config('booking_system.settings')->getRawData();
     $data["number"] = (int) $config["number_of_persons"];
-    $entities = $this->em->getStorage('booking_reservation')->loadMultiple(); //getQuery permet de construire une requête
+    $entities = $this->entityTypeManager->getStorage('booking_reservation')->loadMultiple();
     $today = strtotime('today');
     /**
      *
      * @var \Drupal\booking_system\Entity\BookingReservation $entity
      */
-    foreach($entities as $entity){
-      
+    foreach ($entities as $entity) {
       $reservation_date = $entity->get("reservation_date")->getValue();
       $time_of_reservation = $entity->get("time_of_reservation")->getValue();
-      
       if ($reservation_date && $time_of_reservation) {
         $reservation_date = $reservation_date[0]['value'];
         $time_of_reservation = $time_of_reservation[0]['value'];
-        if($reservation_date == $day && $time_of_reservation == $hour){
-          $data["number"] -= (int)$entity->get("number_of_places")->getValue()[0]['value'];
+        if ($reservation_date == $day && $time_of_reservation == $hour) {
+          $data["number"] -= (int) $entity->get("number_of_places")->getValue()[0]['value'];
         }
       }
     }
     return $data;
   }
+
 }
