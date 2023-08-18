542e28bf496fd4266b2ab54ae9d1c490737a657c
vystii <ngahabanda@gmail.com>
2023-08-18 14:55:20 +0100

    maj des class de margin et padding de l'application

--- src/Controller/BookingSystemController.php
+++ src/Controller/BookingSystemController.php
@@ -15,10 +15,12 @@ use Drupal\Component\Serialization\Json;
 /**
  * Returns responses for booking_system routes.
  */
-class BookingSystemController extends ControllerBase {
+class BookingSystemController extends ControllerBase
+{
   protected $manager;
 
-  public function __construct(BookingManagerService $manager) {
+  public function __construct(BookingManagerService $manager)
+  {
     $this->manager = $manager;
   }
 
@@ -26,14 +28,16 @@ class BookingSystemController extends ControllerBase {
    *
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container)
+  {
     return new static($container->get('booking_system.manager'));
   }
 
   /**
    * Builds the response to showing the Vue-js app
    */
-  public function build() {
+  public function build()
+  {
     $build['content'] = [
       '#type' => 'html_tag',
       '#tag' => 'section',
@@ -53,15 +57,16 @@ class BookingSystemController extends ControllerBase {
   /**
    * Builds the response to showing the Vue-js app
    */
-  public function buildV2() {
+  public function buildV2()
+  {
     $build['content'] = [
       '#type' => 'html_tag',
       '#tag' => 'section',
       "#attributes" => [
         'id' => 'app',
         'class' => [
-          'm-5',
-          'p-5'
+          'm-md-5',
+          'p-md-5'
         ]
       ]
     ];
@@ -72,13 +77,13 @@ class BookingSystemController extends ControllerBase {
   /**
    * Give the days to disable
    */
-  public function dates() {
+  public function dates()
+  {
     try {
       $data = $this->manager->generateDates();
       $data['disabledDates'] = $this->manager->generateDisabledDates();
       return HttpResponse::response($data);
-    }
-    catch (\Exception $e) {
+    } catch (\Exception $e) {
       $errors = ExceptionExtractMessage::errorAll($e);
       $this->getLogger('boobooking_system.settingsking_system')->critical(ExceptionExtractMessage::errorAllToString($e));
       return HttpResponse::response($errors, 400, $e->getMessage());
@@ -92,7 +97,8 @@ class BookingSystemController extends ControllerBase {
    * @throws \Exception
    * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
-  public function setReservation(Request $request) {
+  public function setReservation(Request $request)
+  {
     try {
       if (\Drupal::currentUser()->id()) {
         /**
@@ -104,8 +110,7 @@ class BookingSystemController extends ControllerBase {
         return HttpResponse::response($datas);
       }
       throw new \Exception("Vous n'etes pas connecté(e)");
-    }
-    catch (\Exception $e) {
+    } catch (\Exception $e) {
       $errors = ExceptionExtractMessage::errorAllToString($e);
       $this->getLogger('booking_system')->critical($e->getMessage() . '<br>' . $errors);
       return HttpResponse::response(ExceptionExtractMessage::errorAll($e), 400, $e->getMessage());
@@ -115,7 +120,8 @@ class BookingSystemController extends ControllerBase {
   /**
    * Give the differents schedule of a day
    */
-  public function schedule($day) {
+  public function schedule($day)
+  {
     $day = (int) $day;
 
     $data = $this->manager->generateSchdules($day);
@@ -130,7 +136,8 @@ class BookingSystemController extends ControllerBase {
    *
    * {@inheritdoc} return the the number of seat left
    */
-  public function getSeatsNumber($day, $hour) {
+  public function getSeatsNumber($day, $hour)
+  {
     $day = (int) $day;
     $data = $this->manager->getSeats($day, $hour);
     if (isset($data['error'])) {
@@ -143,12 +150,12 @@ class BookingSystemController extends ControllerBase {
    *
    * @inheritdoc
    */
-  public function default() {
+  public function default()
+  {
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t('Starting default page for testing purpose!')
     ];
     return $build;
   }
-
 }
